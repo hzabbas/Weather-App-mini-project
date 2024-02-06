360a902c3e3afc543fc04ea667cb9861dcbd0f64
Abbas hajizadeh <125821045+hzabbas@users.noreply.github.com>
2024-02-06 12:13:43 +0330

    Adding weather information of cities

--- main.js
+++ main.js
@@ -25,3 +25,35 @@ document.addEventListener("DOMContentLoaded", function () {
     // Change background every 5 seconds
     setInterval(changeBackground, 5000);
 });
+
+let citiesData = [
+  {city: 'Tehran', temp: 12, weather: 'Sunny', humidity: 23, windSpeed: 32},
+  {city: 'Shiraz', temp: 9, weather: 'Cloudy', humidity: 12, windSpeed: 14},
+  {city: 'Tabriz', temp: 1, weather: 'rainy', humidity: 43, windSpeed: 12},
+  {city: 'Mashhad', temp: 16, weather: 'snowy', humidity: 7, windSpeed: 24},
+  {city: 'Esfahan', temp: 23, weather: 'Sunny', humidity: 15, windSpeed: 18},
+]
+
+let inputWeather = document.querySelector(".Search__input-Tag")
+let SearchBtn = document.querySelector(".Search__Icon")
+
+
+SearchBtn.addEventListener("click" , function(){
+    let searchValue = inputWeather.value
+    let mainCityDatas = citiesData.find(function(item){
+        return item.city === searchValue
+    });
+    //console.log(mainCityDatas)
+    let $ = document ;
+    if(mainCityDatas){
+        $.querySelector(".Weather_status").style.display = "inline"
+        $.querySelector(".Load_title").style.display = "none"
+        $.querySelector(".City__title").innerHTML = "Weather IN "+ mainCityDatas.city + " :)"
+        $.querySelector(".Temp__status").innerHTML = mainCityDatas.temp 
+        $.querySelector(".Weather__title-status").innerHTML = mainCityDatas.weather
+        $.querySelector(".Humidity").innerHTML = "Humidity : " + mainCityDatas.humidity + "%"
+        $.querySelector(".wind__speed").innerHTML = "Wind Speed : " + mainCityDatas.windSpeed + "Km/h"
+    }else{
+        alert("The desired city was not found!")
+    }
+})
